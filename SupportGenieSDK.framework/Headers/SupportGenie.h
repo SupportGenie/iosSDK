9e3f457f0c4d26c179a4b5cc361ac09ac62512ba
tarun <agumbe22@gmail.com>
2019-10-07 07:39:16 -0700

    created new files

---
+++ SupportGenieSDK.framework/Headers/SupportGenie.h
@@ -0,0 +1,23 @@
+//
+//  SupportGenie.h
+//  kingfisher
+//
+//  Created by tarun mehta on 3/6/19.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SupportGenie : NSObject
+
+//@property (readonly) NSPersistentContainer * persistentContainer;
+
++ (SupportGenie *)instance;
+- (void) start;
+- (void) applicationDidBecomeActive;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SupportGenieSDK.framework/Headers/SupportGenieSDK.h
@@ -0,0 +1,19 @@
+//
+//  SupportGenieSDK.h
+//  SupportGenieSDK
+//
+//  Created by tarun mehta on 10/4/19.
+//  Copyright © 2019 SupportGenie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SupportGenieSDK.
+FOUNDATION_EXPORT double SupportGenieSDKVersionNumber;
+
+//! Project version string for SupportGenieSDK.
+FOUNDATION_EXPORT const unsigned char SupportGenieSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SupportGenieSDK/PublicHeader.h>
+
+
