f5e2d1521cc76f26d08ad5178fb6a553aeb4e95d
tarun <agumbe22@gmail.com>
2020-09-10 13:31:16 +0530

    batch file to update cocoapods

---
+++ updateSDK.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+rm -rf SupportGenieSDK.framework
+cp -R ../iosSDKDev/SupportGenieSDK/build/universal/SupportGenieSDK.framework ./
+git add SupportGenieSDK.framework
+git add SupportGenieSDK.podspec
+git commit -m "added JWT auth"
+git push origin
+git tag v0.0.184
+git push origin --tags
+pod trunk push SupportGenieSDK.podspec --allow-warnings
+
+
