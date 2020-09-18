0cfa4755a4f6d1a487c22c277b13e4a6a37dc8e3
satya <satya.p91@yahoo.com>
2020-09-18 13:39:58 +0530

    added new code for videoCall using videoSessionId

--- updateSDK.sh
+++ updateSDK.sh
@@ -3,9 +3,10 @@ rm -rf SupportGenieSDK.framework
 cp -R ../iosSDKDev/SupportGenieSDK/build/universal/SupportGenieSDK.framework ./
 git add SupportGenieSDK.framework
 git add SupportGenieSDK.podspec
-git commit -m "added JWT auth"
+git add updateSDK.sh
+git commit -m "added new code for videoCall using videoSessionId"
 git push origin
-git tag v0.0.186
+git tag v0.0.198
 git push origin --tags
 pod trunk push SupportGenieSDK.podspec --allow-warnings
 
