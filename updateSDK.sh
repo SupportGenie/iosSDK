edcd323b118c9f451735dff3024b03f2b3f3792e
satya <satya.p91@yahoo.com>
2020-10-07 12:03:29 +0530

    added new code for turn/sturn server url and improve video call

--- updateSDK.sh
+++ updateSDK.sh
@@ -4,9 +4,9 @@ cp -R ../iosSDKDev/SupportGenieSDK/build/universal/SupportGenieSDK.framework ./
 git add SupportGenieSDK.framework
 git add SupportGenieSDK.podspec
 git add updateSDK.sh
-git commit -m "added new code for videoCall using videoSessionId"
+git commit -m "added new code for turn/sturn server url and improve video call"
 git push origin
-git tag v0.0.228
+git tag v0.0.235
 git push origin --tags
 pod trunk push SupportGenieSDK.podspec --allow-warnings
 
